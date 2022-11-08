74d5d0fed551df67e8fe89d4d8656c2cdc15c6ae
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-08 17:55:37 +0100

    name output dom js done

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
 // Calcolo prezzo biglietto del viaggio in base all'età e ai km da percorrere 2.0
 
 // Dichiarazioni variabili
-const nomeInput = document.getElementById("nomeInput");
-const nomeCognome = nomeInput.value;
 
-const kmInput = document.getElementById("kmInput");
-const kmViaggio = kmInput.value;
-
-const etaInput = document.getElementById("etaInput");
-const fasciaEta = etaInput.value;
 
 const prezzoAlKm = 0.21;
 let prezzoViaggio;
@@ -25,6 +18,16 @@ const annulla = document.querySelector("#annulla");
 // Bottone "Genera"
 genera.addEventListener("click",
     function() {
+        // Dichiarazioni costanti per il bottone
+        const nomeInput = document.getElementById("nomeInput");
+        const nomeCognome = nomeInput.value;
+
+        const kmInput = document.getElementById("kmInput");
+        const kmViaggio = kmInput.value;
+
+        const etaInput = document.getElementById("etaInput");
+        const fasciaEta = etaInput.value;
+
         // Moltiplicare km X 0.21€
         prezzoViaggio = prezzoAlKm * kmViaggio;
 
@@ -32,15 +35,20 @@ genera.addEventListener("click",
         if(fasciaEta === "minorenne"){
             sconto = prezzoViaggio * 0.20;
             prezzoViaggio -= sconto;
+            console.log(prezzoViaggio)
 
         } else if (fasciaEta === "over65"){// Altrimenti se è over65 sconto del 40%
             sconto = prezzoViaggio * 0.40;
             prezzoViaggio -= sconto;
+            console.log(prezzoViaggio)
+
 
         }
 
+        // Inserimento nome passeggero
+        document.getElementById("nomePasseggero").innerHTML = nomeCognome;
         // Output del prezzo finale (massimo 2 decimali)
-        document.getElementById("prezzo").innerHTML = `Il prezzo del tuo biglietto è di ${prezzoViaggio.toFixed(2)}€`;  
+        document.getElementById("costoBiglietto").innerHTML = prezzoViaggio.toFixed(2) + "€";
 
         // Pulizia input
         nomeInput.value = null;
