3f4868fc2c86924b4de91622342bc476fc562c6f
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-08 19:18:29 +0100

    All Project Complete

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 // Calcolo prezzo biglietto del viaggio in base all'età e ai km da percorrere 2.0
 
 // Dichiarazioni variabili
-
-
 const prezzoAlKm = 0.21;
 let prezzoViaggio;
 let sconto;
-
+const nCasuale = Math.floor (Math.random() * 10) + 1;
 const genera = document.querySelector("#genera");
 const annulla = document.querySelector("#annulla");
+const blockOutput = document.querySelector(".outputBlock");
 
 // Pulizia input iniziale
         nomeInput.value = null;
@@ -28,7 +27,11 @@ genera.addEventListener("click",
         const etaInput = document.getElementById("etaInput");
         const fasciaEta = etaInput.value;
 
-        // Moltiplicare km X 0.21€
+        // Rendiamo visibile blocco output
+        blockOutput.classList.remove("d-none");
+
+        if(nomeCognome != "" || kmViaggio != ""){
+            // Moltiplicare km X 0.21€
         prezzoViaggio = prezzoAlKm * kmViaggio;
 
         // Se è minorenne sconto del 20%
@@ -41,19 +44,34 @@ genera.addEventListener("click",
             sconto = prezzoViaggio * 0.40;
             prezzoViaggio -= sconto;
             console.log(prezzoViaggio)
-
-
         }
 
-        // Inserimento nome passeggero
+        // Output
+
+        // Nome passeggero
         document.getElementById("nomePasseggero").innerHTML = nomeCognome;
-        // Output del prezzo finale (massimo 2 decimali)
+
+        // Offerta
+        document.getElementById("offerta").innerHTML = "Biglietto Standard";
+
+        // Numero Carrozza
+        document.getElementById("nCarrozza").innerHTML = nCasuale;
+
+        // Codice CP
+        document.getElementById("codiceCP").innerHTML = "92911";
+
+        // Prezzo finale (massimo 2 decimali)
         document.getElementById("costoBiglietto").innerHTML = prezzoViaggio.toFixed(2) + "€";
 
         // Pulizia input
         nomeInput.value = null;
         kmInput.value = null;
         etaInput.value = null;
+        } else{
+            alert("Compila i campi per procedere!")
+            blockOutput.classList.add("d-none");
+        }
+        
     }
 )
 
@@ -64,10 +82,25 @@ annulla.addEventListener("click",
         nomeInput.value = null;
         kmInput.value = null;
         etaInput.value = null;
+
+        // Rendiamo visibile blocco output
+        blockOutput.classList.add("d-none");
+
+        // Pulizia output
+
+        // Nome passeggero
+        document.getElementById("nomePasseggero").innerHTML = null;
+
+        // Prezzo finale (massimo 2 decimali)
+        document.getElementById("costoBiglietto").innerHTML = null;
+
+        // Offerta
+        document.getElementById("offerta").innerHTML = null;
+
+        // Numero Carrozza
+        document.getElementById("nCarrozza").innerHTML = null;
+
+        // Codice CP
+        document.getElementById("codiceCP").innerHTML = null;
     }
 )
-
-
-
-
-
