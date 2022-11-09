3de0860c8916edfe230aaa690fa0c2756b547cd9
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-09 09:31:40 +0100

    bug fix

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ const nCasuale = Math.floor (Math.random() * 10) + 1;
 const genera = document.querySelector("#genera");
 const annulla = document.querySelector("#annulla");
 const blockOutput = document.querySelector(".outputBlock");
+let n = 0;
 
 // Pulizia input iniziale
         nomeInput.value = null;
@@ -16,7 +17,10 @@ const blockOutput = document.querySelector(".outputBlock");
 
 // Bottone "Genera"
 genera.addEventListener("click",
+
     function() {
+        if (n == 0){
+        n = 1;
         // Dichiarazioni costanti per il bottone
         const nomeInput = document.getElementById("nomeInput");
         const nomeCognome = nomeInput.value;
@@ -30,54 +34,58 @@ genera.addEventListener("click",
         // Rendiamo visibile blocco output
         blockOutput.classList.remove("d-none");
 
+        // Se l'utente lascia vuoti i campo non può procedere
         if(nomeCognome != "" || kmViaggio != ""){
             // Moltiplicare km X 0.21€
-        prezzoViaggio = prezzoAlKm * kmViaggio;
+             prezzoViaggio = prezzoAlKm * kmViaggio;
 
-        // Se è minorenne sconto del 20%
-        if(fasciaEta === "minorenne"){
-            sconto = prezzoViaggio * 0.20;
-            prezzoViaggio -= sconto;
-            console.log(prezzoViaggio)
+            // Se è minorenne sconto del 20%
+            if(fasciaEta === "minorenne"){
+                sconto = prezzoViaggio * 0.20;
+                prezzoViaggio -= sconto;
+                console.log(prezzoViaggio)
 
-        } else if (fasciaEta === "over65"){// Altrimenti se è over65 sconto del 40%
-            sconto = prezzoViaggio * 0.40;
-            prezzoViaggio -= sconto;
-            console.log(prezzoViaggio)
-        }
+            } else if (fasciaEta === "over65"){// Altrimenti se è over65 sconto del 40%
+                sconto = prezzoViaggio * 0.40;
+                prezzoViaggio -= sconto;
+                console.log(prezzoViaggio)
+            }
 
-        // Output
+            // Output
 
-        // Nome passeggero
-        document.getElementById("nomePasseggero").innerHTML = nomeCognome;
+            // Nome passeggero
+            document.getElementById("nomePasseggero").innerHTML = nomeCognome;
 
-        // Offerta
-        document.getElementById("offerta").innerHTML = "Biglietto Standard";
+            // Offerta
+            document.getElementById("offerta").innerHTML = "Biglietto Standard";
 
-        // Numero Carrozza
-        document.getElementById("nCarrozza").innerHTML = nCasuale;
+            // Numero Carrozza
+            document.getElementById("nCarrozza").innerHTML = nCasuale;
 
-        // Codice CP
-        document.getElementById("codiceCP").innerHTML = "92911";
+            // Codice CP
+            document.getElementById("codiceCP").innerHTML = "92911";
 
-        // Prezzo finale (massimo 2 decimali)
-        document.getElementById("costoBiglietto").innerHTML = prezzoViaggio.toFixed(2) + "€";
+            // Prezzo finale (massimo 2 decimali)
+            document.getElementById("costoBiglietto").innerHTML = prezzoViaggio.toFixed(2) + "€";
 
-        // Pulizia input
-        nomeInput.value = null;
-        kmInput.value = null;
-        etaInput.value = null;
-        } else{
-            alert("Compila i campi per procedere!")
-            blockOutput.classList.add("d-none");
-        }
+            // Pulizia input
+            nomeInput.value = null;
+            kmInput.value = null;
+            etaInput.value = null;
+            } else{
+                alert("Compila i campi per procedere!")
+                blockOutput.classList.add("d-none");
+            }
         
+        }
     }
+        
 )
 
 // Bottone "Cancella"
 annulla.addEventListener("click",
     function() {
+        n = 0;
         // Pulizia input
         nomeInput.value = null;
         kmInput.value = null;
